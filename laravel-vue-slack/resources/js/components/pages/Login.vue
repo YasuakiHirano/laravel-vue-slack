db2dfdf76ed94171e2bc0adf05d25982b485cd3f
y.hirano <yasuakihappy@gmail.com>
2021-08-09 19:00:35 +0900

    login screen add vuelidate

--- laravel-vue-slack/resources/js/components/pages/Login.vue
+++ laravel-vue-slack/resources/js/components/pages/Login.vue
@@ -4,32 +4,55 @@
     <sign-in-text>Sign in to laravel-vue-slack.</sign-in-text>
     <div class="w-96 m-auto text-left pt-8">
       <form-label class="mb-2" forText="email" showText="Email address" />
-      <form-text  class="mb-2" type="text" v-model="email" name="email" id="email" placeholder="name@example.com" />
+      <form-text  class="mb-2" type="text" v-model="emailAddress" name="email" id="email" placeholder="name@example.com" :class="{ 'ring-2' : v$.emailAddress.$error,  'ring-red-500' : v$.emailAddress.$error, }" />
+      <div v-for="(error, index) in v$.emailAddress.$errors" :key="index" class="text-xs text-red-500 mb-2">
+        <div v-if="error.$validator == 'required'">メールアドレスは必須です。</div>
+        <div v-if="error.$validator == 'email'">メールアドレスの形式が正しくありません。</div>
+      </div>
       <form-label class="mb-2" forText="password" showText="Password" />
-      <form-text  class="mb-5" type="password" v-model="password" name="password" id="password" placeholder="Your password" />
-      <sign-in-button @event:SignIn="signIn" />
+      <form-text type="password" v-model="password" name="password" id="password" placeholder="Your password" :class="{ 'ring-2' : v$.password.$error,  'ring-red-500' : v$.password.$error, }" />
+      <div v-for="(error, index) in v$.password.$errors" :key="index" class="text-xs text-red-500 mt-2">
+        <div v-if="error.$validator == 'required'">パスワードは必須です。</div>
+      </div>
+      <sign-in-button class="mt-5" @event:SignIn="signIn" />
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import router from '../../router/index.js'
-import { UserSignIn, FetchUsers } from '../../apis/signIn.api.js'
+import { UserSignIn } from '../../apis/signIn.api.js'
+import { useVuelidate } from '@vuelidate/core'
+import { email, required } from '@vuelidate/validators'
 
 export default {
   setup() {
+    const emailAddress = ref('')
+    const password = ref('')
 
     const signIn = async () => {
-      const isSignIn = await UserSignIn(email.value, password.value)
+      const isFormCorrect = await v$.value.$validate()
+      if (!isFormCorrect) return
+
+      const isSignIn = await UserSignIn(emailAddress.value, password.value)
       if (isSignIn) {
         router.push({ path: 'chat' })
-        // await FetchUsers()
       }
     }
 
+    const rules = {
+      emailAddress: { required, email },
+      password: { required }
+    }
+
+    const v$ = useVuelidate(rules, { emailAddress, password })
 
     return {
-      signIn
+      signIn,
+      emailAddress,
+      password,
+      v$
     }
   }
 }
